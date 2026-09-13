d21e1bc6b16431904bb2a8ab0de73db34670da20
assem32 <assemhadidi74@gmail.com>
2026-09-13 13:50:30 +0300

    feature: deploy with github page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Gowwa-Al-Masna3/',
   server: {
     allowedHosts: [
       'envision-unsettled-oxidize.ngrok-free.dev'
